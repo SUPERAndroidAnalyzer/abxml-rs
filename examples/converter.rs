46b79f5d6eaf0e8ba8b508386c4d15c6b963e80b
Guillem Nieto <guillem.nieto@socialpoint.es>
2016-11-26 21:47:52 +0100

    Added command to convert a target file

---
+++ examples/converter.rs
@@ -0,0 +1,47 @@
+extern crate abxml;
+
+use std::env;
+use abxml::encoder::Xml;
+use abxml::BinaryXmlDecoder;
+use std::path::Path;
+use std::fs::File;
+use std::error::Error;
+use std::io::prelude::*;
+
+
+fn main() {
+    let path = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Usage: converter <path>");
+            return;
+        }
+    };
+
+    let content = file_get_contents(&path);
+    let parser = BinaryXmlDecoder::new(&content);
+    let result = parser.decode().unwrap();
+    let xml_content = Xml::encode(&result.root_element).unwrap();
+    println!("{}", xml_content);
+}
+
+fn file_get_contents(path: &str) -> Vec<u8> {
+    let path = Path::new(path);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut v: Vec<u8> = Vec::new();
+    match file.read_to_end(&mut v) {
+        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Ok(_) => (),
+    };
+
+    return v;
+}
