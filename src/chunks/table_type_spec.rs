681a56a762df90290455bb699fdcffed19f02ccd
Guillem Nieto <gnieto.talo@gmail.com>
2016-12-06 23:15:27 +0100

    PackageChunk fully parsed

---
+++ src/chunks/table_type_spec.rs
@@ -0,0 +1,25 @@
+use std::io::{Error, ErrorKind};
+use chunks::{Chunk, ChunkHeader};
+use std::io::Cursor;
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::rc::Rc;
+use document::{HeaderStringTable, StringTable};
+
+pub struct TableTypeSpecDecoder;
+
+impl TableTypeSpecDecoder {
+    pub fn decode(cursor: &mut Cursor<&[u8]>, header: &ChunkHeader)  -> Result<Chunk, Error> {
+        let id = cursor.read_u32::<LittleEndian>()? & 0xFF;
+        let resource_count = cursor.read_u32::<LittleEndian>()?;
+
+        let mut resources = Vec::new();
+
+        for _ in 0..resource_count {
+            resources.push(cursor.read_u32::<LittleEndian>()?);
+        }
+
+        //println!("{:?}", resources);
+
+        Ok(Chunk::TableTypeSpec(id, resources))
+    }
+}
