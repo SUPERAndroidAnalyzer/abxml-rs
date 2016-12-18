5bdb45ae8f18266b1144716d04fe100e660f3849
Guillem Nieto <guillem.nieto@socialpoint.es>
2016-12-18 16:07:19 +0100

    Unified decoders

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,11 +2,20 @@ use chunks::{Chunk, ChunkLoader};
 use std::io::Cursor;
 use byteorder::{LittleEndian, ReadBytesExt};
 use errors::*;
+use document::StringTable;
 
-pub struct ArscDecoder;
+pub struct Decoder {
+    string_table: Option<StringTable>,
+}
 
-impl ArscDecoder {
-    pub fn decode(&self, raw_data: &[u8]) -> Result<Vec<Chunk>> {
+impl Decoder {
+    pub fn new() -> Self {
+        Decoder {
+            string_table: None,
+        }
+    }
+
+    pub fn decode_arsc(&self, raw_data: &[u8]) -> Result<Vec<Chunk>> {
         let mut cursor = Cursor::new(raw_data);
 
         let token = cursor.read_u16::<LittleEndian>()?;
@@ -18,17 +27,14 @@ impl ArscDecoder {
 
         ChunkLoader::read_all(&mut cursor, chunk_size as u64)
     }
-}
 
-pub struct XmlDecoder;
-
-impl XmlDecoder {
-    pub fn decode(&self, raw_data: &[u8]) -> Result<Vec<Chunk>> {
+    pub fn decode_xml(&self, raw_data: &[u8]) -> Result<Vec<Chunk>> {
         let mut cursor = Cursor::new(raw_data);
 
         let token = cursor.read_u16::<LittleEndian>()?;
         let header_size = cursor.read_u16::<LittleEndian>()?;
         let chunk_size = cursor.read_u32::<LittleEndian>()?;
+        let package_amount = cursor.read_u32::<LittleEndian>()?;
 
         info!("Parsing resources.arsc. Buffer size: {}", raw_data.len());
 
