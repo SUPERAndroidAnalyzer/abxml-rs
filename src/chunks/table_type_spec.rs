207a29402746b9db1a0ac89e0c791f31fc9d7045
Guillem Nieto <gnieto.talo@gmail.com>
2017-01-13 18:44:18 +0100

    Coupled decoder to table type and table type spec

--- src/chunks/table_type_spec.rs
+++ src/chunks/table_type_spec.rs
@@ -4,11 +4,12 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use std::rc::Rc;
 use document::{HeaderStringTable, StringTable};
 use errors::*;
+use parser::Decoder;
 
 pub struct TableTypeSpecDecoder;
 
 impl TableTypeSpecDecoder {
-    pub fn decode(cursor: &mut Cursor<&[u8]>, header: &ChunkHeader)  -> Result<Chunk> {
+    pub fn decode(decoder: &mut Decoder, cursor: &mut Cursor<&[u8]>, header: &ChunkHeader)  -> Result<Chunk> {
         let id = cursor.read_u32::<LittleEndian>()? & 0xFF;
         let resource_count = cursor.read_u32::<LittleEndian>()?;
 
@@ -19,7 +20,22 @@ impl TableTypeSpecDecoder {
         }
 
         //println!("{:?}", resources);
+        decoder.push_type_spec(Chunk::TableTypeSpec(id, resources.clone()));
 
         Ok(Chunk::TableTypeSpec(id, resources))
+    }
+}
+
+struct TypeSpec {
+    id: u8,
+    resources: Vec<u32>,
+}
+
+impl TypeSpec {
+    pub fn new(id: u8, resources: Vec<u32>) -> Self {
+        TypeSpec {
+            id: id,
+            resources: resources,
+        }
     }
 }
