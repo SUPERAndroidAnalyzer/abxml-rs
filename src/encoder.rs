66b4f29c496a12817d30a7c65c6f870a0ebcd802
Guillem Nieto <guillem.nieto@socialpoint.es>
2017-02-17 00:49:11 +0100

    Removed all unwraps

--- src/encoder.rs
+++ src/encoder.rs
@@ -41,10 +41,10 @@ impl Xml {
 
             let val = match *a.get_value() {
                 Value::ReferenceId(ref id) => {
-                    Self::resolve_reference(*id, resources, "@")
+                    Self::resolve_reference(*id, resources, "@").ok()
                 }
                 Value::AttributeReferenceId(ref id)=> {
-                    Self::resolve_reference(*id, resources, "?")
+                    Self::resolve_reference(*id, resources, "?").ok()
                 },
                 Value::Integer(ref value) |
                 Value::Flags(ref value)=> {
@@ -78,16 +78,16 @@ impl Xml {
         Ok(())
     }
 
-    fn resolve_reference(id: u32, resources: &Resources, prefix: &str) -> Option<String> {
+    fn resolve_reference(id: u32, resources: &Resources, prefix: &str) -> Result<String> {
         let res_id = id;
         let package_id = id.get_package();
 
         if id == 0 {
-            return Some("@null".to_string());
+            return Ok("@null".to_string());
         }
 
         let is_main = resources.is_main_package(package_id);
-        let package = resources.get_package(package_id);
+        let package = resources.get_package(package_id)?;
         let mut package_borrow = package.borrow_mut();
 
         let entry_key = package_borrow
@@ -105,7 +105,7 @@ impl Xml {
             return package_borrow.format_reference(id, key, namespace, prefix);
         }
 
-        None
+        Err("Error resolving reference".into())
     }
 
     fn resolve_flags(flags: u32, attribute: &Attribute, xml_resources: &[u32], resources: &Resources) -> Option<String> {
@@ -119,111 +119,113 @@ impl Xml {
             };
 
             let package_id = entry_ref.get_package();
-            let package = resources.get_package(package_id as u8);
+            if let Ok(package) = resources.get_package(package_id as u8) {
+                let str_indexes = {
+                    let mut strs = Vec::new();
+                    let mut masks = Vec::new();
 
-            let str_indexes = {
-                let mut strs = Vec::new();
-                let mut masks = Vec::new();
+                    let pb = package.borrow();
 
-                let pb = package.borrow();
+                    let inner_entries = pb.get_entry(*entry_ref)
+                        .and_then(|e| e.complex())
+                        .and_then(|c| Ok(c.get_entries().to_vec()))
+                        .unwrap_or(Vec::new());
+                    let mut sorted = inner_entries.to_vec();
 
-                let inner_entries = pb.get_entry(*entry_ref)
-                    .and_then(|e| e.complex())
-                    .and_then(|c| Ok(c.get_entries().to_vec()))
-                    .unwrap_or(Vec::new());
-                let mut sorted = inner_entries.to_vec();
+                    sorted.sort_by(|a, b| {
+                        let id_a = a.get_value();
+                        let id_b = b.get_value();
 
-                sorted.sort_by(|a, b| {
-                    let id_a = a.get_value();
-                    let id_b = b.get_value();
+                        // TODO: This code is to create an exact match with Apktool. A simple descending ordering seems to be also ok.
+                        let mut i = id_a;
+                        i = i - ((i >> 1) & 0x55555555);
+                        i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+                        i = (i + (i >> 4)) & 0x0f0f0f0f;
+                        i = i + (i >> 8);
+                        i = i + (i >> 16);
+                        i = i & 0x3f;
 
-                    // TODO: This code is to create an exact match with Apktool. A simple descending ordering seems to be also ok.
-                    let mut i = id_a;
-                    i = i - ((i >> 1) & 0x55555555);
-                    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-                    i = (i + (i >> 4)) & 0x0f0f0f0f;
-                    i = i + (i >> 8);
-                    i = i + (i >> 16);
-                    i = i & 0x3f;
+                        let mut j = id_b;
+                        j = j - ((j >> 1) & 0x55555555);
+                        j = (j & 0x33333333) + ((j >> 2) & 0x33333333);
+                        j = (j + (j >> 4)) & 0x0f0f0f0f;
+                        j = j + (j >> 8);
+                        j = j + (j >> 16);
+                        j = j & 0x3f;
 
-                    let mut j = id_b;
-                    j = j - ((j >> 1) & 0x55555555);
-                    j = (j & 0x33333333) + ((j >> 2) & 0x33333333);
-                    j = (j + (j >> 4)) & 0x0f0f0f0f;
-                    j = j + (j >> 8);
-                    j = j + (j >> 16);
-                    j = j & 0x3f;
+                        j.cmp(&i)
+                    });
 
-                    j.cmp(&i)
-                });
+                    for ie in sorted {
+                        let mask = ie.get_value();
 
-                for ie in sorted {
-                    let mask = ie.get_value();
+                        if (mask & flags) == mask {
+                            let maybe_entry = pb.get_entry(ie.get_id());
 
-                    if (mask & flags) == mask {
-                        let maybe_entry = pb.get_entry(ie.get_id());
+                            match maybe_entry {
+                                Ok(entry) => {
+                                    let mut has_to_add = true;
 
-                        match maybe_entry {
-                            Ok(entry) => {
-                                let mut has_to_add = true;
-
-                                for s in masks.iter() {
-                                    if mask & s == mask {
-                                        has_to_add = false;
-                                        break;
+                                    for s in masks.iter() {
+                                        if mask & s == mask {
+                                            has_to_add = false;
+                                            break;
+                                        }
                                     }
-                                }
 
-                                if has_to_add {
-                                    entry.simple()
-                                        .and_then(|s| Ok(s.get_key()))
-                                        .and_then(|key| {
-                                            strs.push(key);
-                                            masks.push(mask);
-                                            Ok(())
-                                        })
-                                        .unwrap_or_else(|_| {
-                                            error!("Value should be added but there was an issue reading the entry");
-                                        });
+                                    if has_to_add {
+                                        entry.simple()
+                                            .and_then(|s| Ok(s.get_key()))
+                                            .and_then(|key| {
+                                                strs.push(key);
+                                                masks.push(mask);
+                                                Ok(())
+                                            })
+                                            .unwrap_or_else(|_| {
+                                                error!("Value should be added but there was an issue reading the entry");
+                                            });
+                                    }
+                                },
+                                Err(_) => {
+                                    info!("Some entry matched but could not found on entries");
                                 }
-                            },
-                            Err(_) => {
-                                info!("Some entry matched but could not found on entries");
                             }
                         }
                     }
+
+                    strs
+                };
+
+                let str_strs: Vec<String> = str_indexes
+                    .iter()
+                    .map(|si| {
+                        let mut pb = package.borrow_mut();
+
+                        match pb.get_entries_string(*si) {
+                            Ok(str) => str,
+                            Err(_) => {
+                                error!("Key not found on the string table");
+
+                                "".to_string()
+                            },
+                        }
+                    })
+                    .collect();
+
+                let final_string = str_strs.join("|");
+
+                if str_strs.is_empty() {
+                    return None;
+                } else {
+                    return Some(final_string);
                 }
-
-                strs
-            };
-
-            let str_strs: Vec<String> = str_indexes
-                .iter()
-                .map(|si| {
-                    let mut pb = package.borrow_mut();
-
-                    match pb.get_entries_string(*si) {
-                        Some(str) => str,
-                        None => {
-                            println!("Key not found on the string table: {}", *si);
-
-                            "".to_string()
-                        },
-                    }
-                })
-                .collect();
-
-            let final_string = str_strs.join("|");
-
-            if str_strs.is_empty() {
-                None
-            } else {
-                Some(final_string)
             }
+
+            return None;
         } else {
             let str = format!("@flags:{}", flags);
 
-            Some(str.to_string())
+            return Some(str.to_string());
         }
     }
 
