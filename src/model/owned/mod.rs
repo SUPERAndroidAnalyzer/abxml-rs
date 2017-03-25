fd1ffcd3fbdcddeadc1644087bd0889d5c1a7aea
Guillem Nieto <gnieto.talo@gmail.com>
2017-03-25 01:00:34 +0100

    Add tests and documentation for tag start

--- src/model/owned/mod.rs
+++ src/model/owned/mod.rs
@@ -22,14 +22,19 @@ mod package;
 mod table_type_spec;
 mod table_type;
 
+/// Implementors are able to be converted to well formed chunks as expected on `ChunkLoaderStream`
 pub trait OwnedBuf {
+    /// Token that identifies the current chunk
     fn get_token(&self) -> u16;
+    /// Return the bytes corresponding to chunk's body
     fn get_body_data(&self) -> Result<Vec<u8>>;
 
+    /// Return the bytes corresponding to chunk's header
     fn get_header(&self) -> Result<Vec<u8>> {
         Ok(Vec::new())
     }
 
+    /// Convert the given `OwnedBuf` to a well formed chunk in form of vector of bytes
     fn to_vec(&self) -> Result<Vec<u8>> {
         let mut out = Vec::new();
         let body = self.get_body_data().chain_err(|| "Could not read chunk body")?;
@@ -41,6 +46,8 @@ pub trait OwnedBuf {
         Ok(out)
     }
 
+    /// Writes the header to the output buffer. It writes token, header size and chunk_size and
+    /// then the data returned by `get_header`.
     fn write_header(&self, buffer: &mut Vec<u8>, body: &[u8]) -> Result<()> {
         let header = self.get_header()?;
         let header_size = header.len() as u16 + 8;
