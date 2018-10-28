7aef0a643130129e981551e8be1161f7c8ea356f
Travis CI User <travis@example.org>
2018-10-28 14:36:04 +0000

    Documentation upload

---
+++ implementors/core/hash/trait.Hasher.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["crc"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"crc/crc16/struct.Digest.html\" title=\"struct crc::crc16::Digest\">Digest</a>",synthetic:false,types:["crc::crc16::Digest"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"crc/crc32/struct.Digest.html\" title=\"struct crc::crc32::Digest\">Digest</a>",synthetic:false,types:["crc::crc32::Digest"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html\" title=\"trait core::hash::Hasher\">Hasher</a> for <a class=\"struct\" href=\"crc/crc64/struct.Digest.html\" title=\"struct crc::crc64::Digest\">Digest</a>",synthetic:false,types:["crc::crc64::Digest"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
